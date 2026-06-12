7fa29693578ac93d9e3230b043128ed8aae99fd6
darcioliveiradeveloper <darcioliveiradeveloper@gmail.com>
2026-06-11 21:27:59 -0300

    rever manutenção

--- src/services/manutencaoService.js
+++ src/services/manutencaoService.js
@@ -1,38 +1,63 @@
 const Manutencao = require('../models/Manutencao');
+const Veiculo = require('../models/Veiculo');
+const Oficina = require('../models/Oficina');
 
-// Criar Manutenção
+// Função auxiliar para calcular o total
+function calcularTotal(services) {
+  return services.reduce((sum, service) => sum + service.price, 0);
+}
+
+// Criar Manutenção com validação e atualização de relacionamentos
 exports.createManutencao = async (data) => {
+  // Verificar se veículo existe
+  const veiculoExistente = await Veiculo.findById(data.veiculo);
+  if (!veiculoExistente) {
+    throw new Error('Veículo não encontrado, verifique o ID informado');
+  }
+
+  // Verificar se oficina existe
+  const oficinaExistente = await Oficina.findById(data.oficina);
+  if (!oficinaExistente) {
+    throw new Error('Oficina não encontrada, verifique o ID informado');
+  }
+
+  // Calcular totalCost
+  data.totalCost = calcularTotal(data.services);
+
+  // Criar manutenção
   const manutencao = new Manutencao(data);
-  return await manutencao.save();
+  await manutencao.save();
+
+  // Atualizar veículo: adicionar manutenção
+  veiculoExistente.maintenances.push(manutencao._id);
+  await veiculoExistente.save();
+
+  // Atualizar oficina: adicionar veículo (se ainda não estiver listado)
+  if (!oficinaExistente.vehicles.includes(veiculoExistente._id)) {
+    oficinaExistente.vehicles.push(veiculoExistente._id);
+    await oficinaExistente.save();
+  }
+
+  return manutencao;
 };
 
-// Listar Manutenções
-exports.getManutencoes = async () => {
-  return await Manutencao.find().populate('veiculo').populate('oficina');
-};
 
-// Listar Manutenção por ID
-exports.getManutencaoById = async (id) => {
-  return await Manutencao.findById(id).populate('veiculo').populate('oficina');
-};
+  // Listar Todas as Manutenção
+  exports.getManutencoes = async () => {
+    return await Manutencao.find();
+  }
+  
+  // Listar Manutenção por ID
+  exports.getManutencaoById = async (id) => {
+    return await Manutencao.findById(id);
+  };
 
-// Atualizar Manutenção
-exports.updateManutencao = async (id, data) => {
-  return await Manutencao.findByIdAndUpdate(id, data, { new: true });
+  // Atualizar Oficina
+exports.updateOficina = async (id, data) => {
+  return await Oficina.findByIdAndUpdate(id, data, { new: true });
 };
 
 // Deletar Manutenção
 exports.deleteManutencao = async (id) => {
   return await Manutencao.findByIdAndDelete(id);
 };
-
-// Buscar Manutenção por Veículo
-exports.getManutencoesByVeiculo = async (veiculoId) => {
-  return await Manutencao.find({ veiculo: veiculoId }).populate('oficina');
-};
-
-// Buscar Manutenção por Oficina
-exports.getManutencoesByOficina = async (oficinaId) => {
-  return await Manutencao.find({ oficina: oficinaId }).populate('veiculo');
-};
-
